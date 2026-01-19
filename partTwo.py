554e2a521864ba6f276ce7e77fad06e4ad7cf025
dwhish <dk440@kent.ac.uk>
2026-01-19 14:25:53 +0000

    finding hypotenuse

--- partTwo.py
+++ partTwo.py
@@ -1,9 +1,15 @@
 import math  
 
 def main():
-#TO DO  
+  A= input(int"what is the length of A")
+  B= input(int"what is the length of B")
+  C= pythag(A,B)
+  print(C)
+
 
 def pythag(A,B):
-#TO DO  
+   csquared= A**2 + B**2
+   math.sqrt(csquared)
+
 
 main()
