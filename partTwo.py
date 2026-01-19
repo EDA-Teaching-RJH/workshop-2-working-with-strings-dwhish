3ebf0e83887f8d76c8c9e1bf3474c62b7560ae7e
dwhish <dk440@kent.ac.uk>
2026-01-19 14:32:29 +0000

    2nd attempt at returning hypotenuse

--- partTwo.py
+++ partTwo.py
@@ -1,15 +1,15 @@
 import math  
 
 def main():
-  A= input(int"what is the length of A")
-  B= input(int"what is the length of B")
+  A= int(input("what is the length of A"))
+  B= int(input("what is the length of B"))
   C= pythag(A,B)
   print(C)
 
 
 def pythag(A,B):
    csquared= A**2 + B**2
-   math.sqrt(csquared)
+   return math.sqrt(csquared)
 
 
 main()
